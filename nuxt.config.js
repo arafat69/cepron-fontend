9b311116a5a4e42d2fd0900397439ee9f1942f75
arafat69 <arafataliar69@gmail.com>
2022-08-23 13:40:51 +0600

    generate

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@
 import i18n from './config/i18n'
 export default {
   mode: 'universal',
+  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'cepron',
